846f6078495534d0f0a90907bbb0fcf1fd469e7f
Luan Guimarães <luan.guimaraes@live.com>
2018-07-02 14:44:14 -0300

    Tela Inicial parte 2

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Game News</title>
     <link rel="stylesheet" href="css/style.css">
   </head>
@@ -12,13 +13,26 @@
         <h1>Game News</h1>
       </div>
       <div id="menu">
-        
+        <a href="">Item 1</a>
+        <a href="">Item 2</a>
+        <a href="">Item 3</a>
       </div>
       <div id="banner">
-
+        <img src="http://placehold.it/1980x720" alt="Place Hold">
       </div>
       <div id="noticias">
-
+        <h2>Noticias</h2>
+        <div class="box">
+          <div class="noticia">
+            <div class="foto">
+              <img src="http://placehold.it/480x320" alt="Placeholder">
+            </div>
+            <div class="conteudo">
+              <h3 class="titulo">Ut enim ad minim veniam</h3>
+              <p class="resumo">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div id="bot">
 
