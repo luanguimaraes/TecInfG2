bd98c05dad87b33db611e390d982a511ddfa5db6
Luan Guimarães <luan.guimaraes@live.com>
2018-07-02 20:09:38 -0300

    Tela Inicial parte 3
    Agora as notícias são buscadas no bando de dados, falta arrumar a responsividade.

--- connection.php
+++ connection.php
@@ -7,12 +7,13 @@
   // Conexão
   $conn = new mysqli($host, $username, $password, $database);
 
+
   // Checar coneção
   if($conn->connect_error){
     die("Conexão falhou: " . $conn->connect_error);
   }
 
-  echo 'Conectado ao banco de dados: ' . $database;
+  // echo 'Conectado ao banco de dados: ' . $database;
   echo '<br />';
 
  ?>
