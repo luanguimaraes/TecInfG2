873b36f0ec5139d0414bc66fe780f92ac3d41c16
Luan Guimarães <luan.guimaraes@live.com>
2018-07-03 14:23:54 -0300

    Revert "Criado a página de formulário"
    This reverts commit e63a739d71e86c25337a113af68e63ceca6d6edd.

--- form.php
+++ form.php
@@ -2,89 +2,59 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>Game News</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/form.css">
+    <title>Formulário</title>
+    <style media="screen">
+      body{
+        background-color: ;
+      }
+    </style>
   </head>
   <body>
-     <div id="interface">  <!-- bug no xampp? -->
-      <div id="top">
-        <img src="http://placehold.it/50x40" alt="Place Hold">
-        <h1>Game News</h1>
-      </div>
-      <div id="menu">
-        <a href="">Home</a>
-        <a href="">Notícias</a>
-        <a href="">Nova Notícia</a>
-      </div>
-      <div id="banner">
-        <img src="img/banner.jpg" alt="Place Hold">
-      </div>
-      <button onclick="topFunction()" id="botao" title="top">^</button>
-
-      <div class="container">
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
-          <label for="fname">Título</label>
-          <input type="text" id="fname" name="titulo">
-
-          <label for="subject">Resumo</label>
-          <textarea id="subject" name="resumo" placeholder="Write something.." style="height:100px"></textarea>
-
-          <label for="subject">Notícia</label>
-          <textarea id="subject" name="noticia" style="height:200px"></textarea>
-
-          <label for="fname">Autor</label>
-          <input type="text" id="fname" name="autor">
-
-          <label for="fname">Imagem</label>
-          <input type="file" id="fname" name="titulo">
-
-          <input type="submit" value="Submit">
-        </form>
-
-        <?php
-    if (isset($_POST['submit'])) {
-          include 'connection.php';
-
-          $titulo = $_POST ["titulo"];
-          $noticia = $_POST ["noticia"];
-          $autor = $_POST ["autor"];
-          $foto = $_FILES["userfile"];
-
-          if (!empty($foto["name"])) {
-            echo 'entrou';
-            preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $foto["name"], $ext);
-            $nome_imagem = md5(uniqid(time())) . "." . $ext[1];
-            $caminho_imagem = "fotos/" . $nome_imagem;
-            move_uploaded_file($foto["tmp_name"], $caminho_imagem);
-          }else{
-            echo 'não entrou';
-            $nome_imagem = '';
-          }
-
-          // Inserir no Banco
-          $sql = "INSERT INTO News (titulo, noticia, autor, nome_imagem, data) VALUES ('$titulo', '$noticia', '$autor', '$nome_imagem', current_timestamp)";
-          if($conn->query($sql) === TRUE){
-            echo 'registro adicionado com sucesso';
-          }else{
-            echo 'Erro: ' . $sql . '<br />' . $conn->error;
-          }
-
-          // Fechar conexão
-          $conn->close();
-    }
-        ?>
-      </div>
 
 
-      <div id="bot">
-        <h2>teste</h2>
-      </div>
-      <div id="footer">
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+      Título: <input type="text" name="titulo"><br />
+      Notícia: <input type="text" name="noticia"><br />
+      Autor: <input type="text" name="autor"><br />
+      Foto: <input type="file" name="userfile" />
+      <input type="submit" name="submit">
+    </form>
 
-      </div>
-    </div>
-    <script src="js/botao.js"></script>
+
+
+    <?php
+if (isset($_POST['submit'])) {
+      include 'connection.php';
+
+      $titulo = $_POST ["titulo"];
+      $noticia = $_POST ["noticia"];
+      $autor = $_POST ["autor"];
+      $foto = $_FILES["userfile"];
+
+      if (!empty($foto["name"])) {
+        echo 'entrou';
+        preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $foto["name"], $ext);
+        $nome_imagem = md5(uniqid(time())) . "." . $ext[1];
+        $caminho_imagem = "fotos/" . $nome_imagem;
+        move_uploaded_file($foto["tmp_name"], $caminho_imagem);
+      }else{
+        echo 'não entrou';
+        $nome_imagem = '';
+      }
+
+      // Inserir no Banco
+      $sql = "INSERT INTO News (titulo, noticia, autor, nome_imagem, data) VALUES ('$titulo', '$noticia', '$autor', '$nome_imagem', current_timestamp)";
+      if($conn->query($sql) === TRUE){
+        echo 'registro adicionado com sucesso';
+      }else{
+        echo 'Erro: ' . $sql . '<br />' . $conn->error;
+      }
+
+      // Fechar conexão
+      $conn->close();
+}
+    ?>
+
+
   </body>
 </html>
