23e9a3ea48280d6b92966a8e85785bff52daed3f
Luan Guimarães <luan.guimaraes@live.com>
2018-07-04 08:28:33 -0300

    Revert "Pág. News"
    This reverts commit 8b5fbde6a22dd5ded32afc3c926288182ca5586c.

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Game News</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
   </head>
   <body>
      <div id="interface">  <!-- bug no xampp? -->
@@ -24,62 +23,6 @@
       <button onclick="topFunction()" id="botao" title="top">^</button>
       <div id="noticias">
         <h2>Noticias</h2>
-
-//News
-
-    <div id="news">
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <h2>Notícias</h2>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12 ">
-            <div class="news">
-              <div class="row">
-                <div class="col-12 col-lg-12 col-xl-12">
-                  <div class="content">
-                    <h4> <em class="autor">Autor: Siclano</em> </h4>
-                    <h3 class="title">Título - Lorem ipsum dolor sit amet,Ut enim ad minim veniam,</h3>
-                    <p class="resumo">Resumo da noticia consectetur adipisicing elit, ut labore et dolore.</p>
-                    <div class="col-12 ">
-                      <div class="picture"><img src="http://placehold.it/960x500" alt="Placeholder"></div>
-                    </div>
-                    <p class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint quae quasi accusantium officia eligendi totam ratione, ex assumenda non voluptas aliquid vitae unde blanditiis, sit expedita adipisci voluptate ab, dolor.Quidem unde, assumenda
-                      aperiam pariatur non repellat nisi earum soluta facilis recusandae. Perspiciatis facere esse ducimus, placeat nemo, deserunt eius fugiat reiciendis, amet quam quas quod quae ipsa quidem architecto.</p>
-                  </div>
-                  <p class="data"><em>Publicado em <time datetime="2018-09-05" pubdate>05 de setembro de 2018</em></time>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-12 ">
-            <div class="news">
-              <div class="row">
-                <div class="col-12 ">
-                  <div class="content"><hr>
-                    <h4> <em class="autor">Autor: Siclano</em> </h4>
-                    <h3 class="title">Título - Lorem ipsum dolor sit amet,Ut enim ad </h3>
-                    <p class="resumo">Resumo da noticia consectetur adipisicing elit, ut  magna aliqua.</p>
-                    <div class="col-12 ">
-                      <div class="picture"><img src="http://placehold.it/960x500" alt="Placeholder"></div>
-                    </div>
-                    <p class="descricao">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint quae quasi accusantium officia eligendi totam ratione, ex assumenda non voluptas aliquid vitae unde blanditiis, sit expedita adipisci voluptate ab, dolor.Quidem unde, assumenda
-                      aperiam pariatur non repellat nisi earum soluta facilis recusandae. Perspiciatis facere esse ducimus, placeat nemo, deserunt eius fugiat reiciendis, amet quam quas quod quae ipsa quidem architecto.</p>
-                  </div>
-                  <p class="data"><em>Publicado em <time datetime="2018-09-05" pubdate>05 de setembro de 2018</em></time><hr>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-
         <div class="box">
           <?php
             include 'connection.php';
